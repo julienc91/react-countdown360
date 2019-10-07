1b2db1c64ba8a611862be24a6efc43b5cb80e38a
julien <git@julienc.io>
2019-10-07 19:15:42 +0200

    complete refactoring

--- src/countdown360.js
+++ src/countdown360.js
@@ -1,236 +1,189 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 class Countdown360 extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      seconds: props.seconds,
-      startedAt: null
+      totalSeconds: props.seconds,
+      secondsLeft: props.seconds * 1000,
+      started: false,
+      lastTick: null
     }
 
-    this.ariaText = React.createRef()
-    this.canvas = React.createRef()
-
     this.interval = null
-    this.settings = {}
-
-    this._draw = this._draw.bind(this)
-    this.start = this.start.bind(this)
+    this.handleTick = this.handleTick.bind(this)
   }
 
   componentDidMount () {
-    this._init()
-  }
-
-  setSettings () {
-    const defaults = {
-      radius: 15.5,
-      strokeStyle: '#477050',
-      strokeWidth: undefined,
-      fillStyle: '#8ac575',
-      fontColor: '#477050',
-      fontFamily: 'sans-serif',
-      fontSize: undefined,
-      fontWeight: 700,
-      autostart: true,
-      seconds: 10,
-      label: ['second', 'seconds'],
-      startOverAfterAdding: true,
-      smooth: false,
-      onComplete: undefined
-    }
-    const settings = { ...defaults, ...this.props }
-    if (!settings.fontSize) {
-      settings.fontSize = settings.radius / 1.2
-    }
-    if (!settings.strokeWidth) {
-      settings.strokeWidth = settings.radius / 4
-    }
-    settings.width = settings.height = (settings.radius + settings.strokeWidth) * 2
-    settings.arcX = settings.arcY = settings.radius + settings.strokeWidth
-
-    this.settings = settings
-  }
-
-  _secondsLeft (secondsElapsed) {
-    const { seconds } = this.state
-    return seconds - secondsElapsed
-  }
-
-  getTimeRemaining () {
-    return this._secondsLeft(this.getElapsedTime())
-  }
-
-  getElapsedTime () {
-    const { startedAt } = this.state
-    return Math.round((new Date().getTime() - startedAt.getTime()) / 1000)
-  }
-
-  extendTimer (value) {
-    value = parseInt(value)
-    const { seconds } = this.state
-    const secondsElapsed = this.getElapsedTime()
-    if (this._secondsLeft(secondsElapsed) + value <= seconds) {
-      const { startedAt } = this.state
-      startedAt.setSeconds(startedAt.getSeconds() + value)
-      this.setState({
-        startedAt: { ...startedAt }
-      })
-    }
-  }
-
-  addSeconds (value) {
-    value = parseInt(value)
-    const { startOverAfterAdding } = this.settings
-    const secondsElapsed = this.getElapsedTime()
-    let { seconds } = this.state
-    if (startOverAfterAdding) {
-      seconds = this._secondsLeft(secondsElapsed) + value
-      this.setState({ seconds }, this.start)
-    } else {
-      seconds += value
-      this.setState({ seconds })
-    }
-  }
-
-  start () {
-    const startedAt = new Date()
-    this.setState({ startedAt }, () => {
-      let timerInterval = 1000
-      const { smooth } = this.props
-      if (smooth) {
-        timerInterval = 16
-      }
-      this.interval = setInterval(this._draw, timerInterval)
-    })
-  }
-
-  stop (callback) {
-    clearInterval(this.interval)
-    if (callback) {
-      callback()
-    }
-  }
-
-  _init () {
-    const { autostart } = this.props
-    this.setSettings()
-
-    if (autostart) {
+    const { autoStart } = this.props
+    if (autoStart) {
       this.start()
     }
   }
 
-  getPen () {
-    const { fillStyle, strokeStyle, strokeWidth } = this.settings
-
-    const pen = this.canvas.current.getContext('2d')
-    pen.lineWidth = strokeWidth
-    pen.strokeStyle = strokeStyle
-    pen.fillStyle = fillStyle
-    pen.textAlign = 'center'
-    pen.textBaseline = 'middle'
-    this._clearRect(pen)
-    return pen
+  componentWillUnmount () {
+    clearInterval(this.interval)
   }
 
-  _clearRect (pen) {
-    const { height, width } = this.settings
-    pen.clearRect(0, 0, width, height)
-  }
-
-  _draw () {
-    const { seconds, startedAt } = this.state
-    const millisElapsed = new Date().getTime() - startedAt.getTime()
-    const secondsElapsed = Math.floor(millisElapsed / 1000)
-    const endAngle = (Math.PI * 3.5) - ((Math.PI * 2) / (seconds * 1000)) * millisElapsed
-    const pen = this.getPen()
-    this._drawCountdownShape(pen, Math.PI * 3.5, false)
-    if (secondsElapsed < seconds) {
-      this._drawCountdownShape(pen, endAngle, true)
-      this._drawCountdownLabel(pen, secondsElapsed)
-    } else {
-      this._drawCountdownLabel(pen, seconds)
-      this.stop()
-      const { onComplete } = this.settings
-      onComplete && onComplete()
+  componentDidUpdate (prevProps) {
+    const { smooth } = this.props
+    if (prevProps.smooth !== smooth) {
+      clearInterval(this.interval)
+      const timerInterval = smooth ? 16 : 1000
+      this.interval = setInterval(this.handleTick, timerInterval)
     }
   }
 
-  _drawCountdownShape (pen, endAngle, drawStroke) {
-    const { arcX, arcY, fillStyle, radius } = this.settings
-
-    pen.fillStyle = fillStyle
-    pen.beginPath()
-    pen.arc(arcX, arcY, radius, Math.PI * 1.5, endAngle, false)
-    pen.fill()
-    if (drawStroke) {
-      pen.stroke()
-    }
+  extendTimer (value) {
+    let { secondsLeft, totalSeconds } = this.state
+    totalSeconds += value
+    secondsLeft = Math.min(secondsLeft, totalSeconds * 1000)
+    this.setState({ secondsLeft, totalSeconds })
   }
 
-  _drawCountdownLabel (pen, secondsElapsed) {
-    const secondsLeft = this.getTimeRemaining()
-    this.ariaText.current.innerHTML = secondsLeft
+  addSeconds (value) {
+    const { secondsLeft, totalSeconds } = this.state
+    value *= 1000
+    this.setState({ secondsLeft: Math.max(Math.min(secondsLeft + value, totalSeconds * 1000), 0) })
+  }
 
-    const { fillStyle, fontColor, fontFamily, fontSize, fontWeight, height, label, width } = this.settings
-    pen.font = `${fontWeight} ${fontSize}px ${fontFamily}`
+  start () {
+    const { secondsLeft, started } = this.state
+    if (started || secondsLeft <= 0) {
+      return
+    }
 
-    const label_ = secondsLeft === 1 ? label[0] : label[1]
-    const x = width / 2
-    const y = (height / 2) - (fontSize / 6.2)
+    const { smooth } = this.props
+    const timerInterval = smooth ? 16 : 1000
+    this.interval = setInterval(this.handleTick, timerInterval)
+    this.setState({ started: true, lastTick: new Date() })
+  }
 
-    pen.fillStyle = fillStyle
-    pen.fillText(secondsLeft + 1, x, y)
-    pen.fillStyle = fontColor
-    pen.fillText(secondsLeft, x, y)
-    pen.font = `normal small-caps ${fontSize / 3}px ${fontFamily}`
-    pen.fillText(label_, width / 2, height / 2 + (fontSize / 2.2))
+  stop () {
+    const { started } = this.state
+    if (!started) {
+      return
+    }
+    clearInterval(this.interval)
+    this.handleTick()
+    this.setState({ started: false, lastTick: null })
+  }
+
+  handleTick () {
+    const { lastTick, secondsLeft, started } = this.state
+    if (!started || secondsLeft <= 0) {
+      return
+    }
+
+    const now = new Date()
+    if (!lastTick) {
+      this.setState({ lastTick: now })
+      return
+    }
+
+    const diff = Math.abs(now - lastTick)
+    const updatedSecondsLeft = Math.max(secondsLeft - diff, 0)
+    this.setState({ lastTick: now, secondsLeft: updatedSecondsLeft }, () => {
+      if (updatedSecondsLeft <= 0) {
+        const { onComplete } = this.props
+        onComplete && onComplete()
+        this.stop()
+      }
+    })
   }
 
   render () {
-    const { height, width } = this.settings
+    const { backgroundColor, borderUnfillColor, borderFillColor, borderWidth, smooth, width } = this.props
+    const { fontFamily, fontSize, fontWeight, unit } = this.props
+
+    const { secondsLeft, totalSeconds } = this.state
+
+    let angle
+    if (!smooth) {
+      angle = (Math.round(secondsLeft / 1000) / totalSeconds) * 360
+    } else {
+      angle = (secondsLeft / (totalSeconds * 1000)) * 360
+    }
+
+    const x = angle >= 180 ? (90 - (360 - angle)) : 90
+    const y = angle >= 180 ? 90 : (-90 + angle)
+
+    console.log(angle, x, y)
+
+    const Wrapper = styled.div`
+      align-items: center;
+      background-image: linear-gradient(${x}deg, ${angle >= 180 ? borderFillColor : borderUnfillColor} 50%, transparent 50%), linear-gradient(${y}deg, ${borderUnfillColor} 50%, ${borderFillColor} 50%);
+      border-radius: 50%;
+      display: flex;
+      height: ${width}px;
+      justify-content: center;
+      width: ${width}px;`
+
+    const Label = styled.div`
+      align-items: center;
+      background: ${backgroundColor};
+      border-radius: 50%;
+      box-sizing: border-box;
+      display: flex;
+      flex-flow: column;
+      font-family: ${fontFamily};
+      font-size: ${fontSize}px;
+      font-weight: ${fontWeight};
+      height: ${width - 2 * borderWidth}px;
+      justify-content: center;
+      text-align: center;
+      text-transform: uppercase;
+      width: ${width - 2 * borderWidth}px;`
+
+    const Unit = styled.div`
+      text-transform: uppercase;
+      font-size: ${fontSize / 3}px;`
+
+    const roundedSecondsLeft = Math.round(secondsLeft / 1000)
+
     return (
-      <canvas width={width} height={height} ref={this.canvas}>
-        <span role="status" aria-live="assertive" ref={this.ariaText}/>
-      </canvas>
+      <Wrapper>
+        <Label>
+          {roundedSecondsLeft}<br />
+          <Unit>{roundedSecondsLeft === 1 ? unit[0] : unit[1]}</Unit>
+        </Label>
+      </Wrapper>
     )
   }
 }
 
 Countdown360.defaultProps = {
-  radius: 15.5,
-  strokeStyle: '#477050',
-  strokeWidth: undefined,
-  fillStyle: '#8ac575',
-  fontColor: '#477050',
+  autoStart: true,
+  backgroundColor: '#fff',
+  borderFillColor: '#f11',
+  borderUnfillColor: '#e6e2e7',
+  borderWidth: 20,
+  fontColor: '#111',
   fontFamily: 'sans-serif',
-  fontSize: undefined,
+  fontSize: 45,
   fontWeight: 700,
-  autostart: true,
-  seconds: 10,
-  label: ['second', 'seconds'],
-  startOverAfterAdding: true,
   smooth: false,
-  onComplete: undefined
+  unit: ['second', 'seconds'],
+  width: 200
 }
 
 Countdown360.propTypes = {
-  radius: PropTypes.number,
-  strokeStyle: PropTypes.string,
-  strokeWidth: PropTypes.number,
-  fillStyle: PropTypes.string,
+  autoStart: PropTypes.bool,
+  backgroundColor: PropTypes.string,
+  borderFillColor: PropTypes.string,
+  borderUnfillColor: PropTypes.string,
+  borderWidth: PropTypes.number,
   fontColor: PropTypes.string,
   fontFamily: PropTypes.string,
   fontSize: PropTypes.number,
   fontWeight: PropTypes.number,
-  autostart: PropTypes.bool,
+  onComplete: PropTypes.func,
   seconds: PropTypes.number.isRequired,
-  label: PropTypes.arrayOf(PropTypes.string),
-  startOverAfterAdding: PropTypes.bool,
   smooth: PropTypes.bool,
-  onComplete: PropTypes.func
+  unit: PropTypes.arrayOf(PropTypes.string),
+  width: PropTypes.number
 }
 
 export default Countdown360
